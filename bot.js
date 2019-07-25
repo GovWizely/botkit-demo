a5fadbc1db8b24dc421f4e9062c19ae17443d8bc
Donna Hogan <ddhogan@users.noreply.github.com>
2019-07-24 20:21:33 -0400

    [EDSPA-13] FAQBot 0.2
    - Adds middlware for parsing intent (only 2 at this point: greeting and searching)
      - Middlware integrates with the Wit.ai NLP intent API
    - update Botkit to v4.5 (released today)
    - update packages for security
    
    Update version of wit.ai middleware
    
    - this enables handers (`controller.on()` to work) and results in a more concise json result from the Wit API (without the deprecation warning)
    - Update readme
    - Source middleware from separate fork

--- bot.js
+++ bot.js
@@ -15,6 +15,10 @@ const { MongoDbStorage } = require('botbuilder-storage-mongodb');
 // Load process.env values from .env file
 require('dotenv').config();
 
+const wit = require('botkit-middleware-witai')({
+    token: process.env.WIT_TOKEN,
+});
+
 let storage = null;
 if (process.env.MONGO_URI) {
     storage = mongoStorage = new MongoDbStorage({
@@ -58,17 +62,19 @@ controller.ready(() => {
     }
 });
 
+/* use the Wit AI middleware to receive messages */
+controller.middleware.receive.use(wit.receive);
+
 const endpoint = "https://api.trade.gov/ita_faqs/search";
-const api_key = "K2w8mVRAu1zSW2aJw6P_3GK1";
+const api_key = process.env.API_KEY;
 
 /* The Query Dialog */
 let DIALOG_ID = 'my_dialog_1';
 let query_dialog = new BotkitConversation(DIALOG_ID, controller);
 
-query_dialog.say(`Please hold while I search with: {{vars.word}}`);
-
-// query_dialog.addAction('query_thread', async(query_dialog, bot) => {
+// query_dialog.say(`Please hold while I search with: {{vars.word}}`);
 query_dialog.before('default', async(query_dialog, bot) => {
+    console.log(`Please hold while I search with: ${query_dialog.vars.word}`);
     return new Promise(function(resolve, reject) {
         fetch(`${endpoint}?api_key=${api_key}&q=${query_dialog.vars.word}`)
         .then((response) => response.json())
@@ -86,21 +92,31 @@ query_dialog.before('default', async(query_dialog, bot) => {
 });
 
 query_dialog.say(`There were <b>{{vars.total}} results</b> <ol>{{#vars.results}}<li><a href={{url}} target="_blank" rel="noopener" rel="noreferrer">{{question}}</a></li>{{/vars.results}}</ol>`);
+query_dialog.say(`You might also be interested in searching the <a href="https://www.export.gov/consolidated_screening_list" target="_blank" rel="noopener" rel="noreferrer">Consolidated Screening List</a>`);
 
 controller.addDialog(query_dialog);
 /* End Dialog */
 
-/* Trigger the dialog the first time */
-controller.on(['hello', 'welcome_back', 'reconnect'], async(bot, message) => {
-    // await bot.beginDialog(DIALOG_ID, {word: 'banks'});
-    await bot.say('What search term would you like to search with? <br> (Try "banks" or "export" for example)');
+/* Welcome the user on connect/reconnect */
+controller.on(['hello'], async(bot, message) => {
+    await bot.reply(message, 'What would you like to know about?');
 });
 
-controller.on('message', async(bot, message) => {
-    await bot.beginDialog(DIALOG_ID, {word: message.text});
+controller.on(['welcome_back', 'reconnect'], async(bot, message) => {
+    await bot.reply(message, 'Welcome back!');
 });
 
-/* Trigger the dialog again */
-// controller.hears(['search', 'query'], 'message', async(bot, message) => {
-//     await bot.beginDialog(DIALOG_ID, {word: message.text});
-// });
+/* interpret messages that seem to be searching for something and parse the keywords to trigger the dialog */
+controller.hears(['.*'], ['message'], async function(bot, message) {
+    try {
+        // console.log(`💡 message.entities: ${JSON.stringify(message.entities)}`);
+        if (!!message.entities.greetings) {
+            await bot.reply(message, 'Hello!  How can I help you?');
+        } else { 
+            await bot.beginDialog(DIALOG_ID, {word: message.entities.search_query[0].value});
+        }    
+    }
+    catch(e) {
+        console.log('⚠️  Caught an error: ', e)
+    }
+});
--- readme.md
+++ readme.md
@@ -1,18 +1,22 @@
 # Demo Chatbot for FAQs API
-
+Currently, the bot can do two things:
+  * You can greet it in a variety of ways and it will greet you back.  
+  * You can type in keywords, phrases, or sentences to search with, and it will retrieve a list of results from the FAQ API, and also suggest the CSL link.
+  * This project is hosted at: https://ita-faq-bot.glitch.me/ ![Glitch Badge](https://badge.glitch.me/ita-faq-bot)
 ## Dependencies
 * Node (I used v12.2.0)
 
 ## Run Locally
-* `git clone` this repo and `cd` into it
-* `npm install`
-* `npm start`
-* Navigate to http://localhost:3000.  Currently, the bot can do 2.5 things.  You can greet it in a variety of ways and it will greet you back.  If you type "query", it'll ask you what search term to use, and then (back in the console, unfortunately) tell you how many results were found.
+1. `git clone` this repo and `cd` into it
+2. `npm install`
+3. `npm start`
+4. Navigate to http://localhost:3000
+5. Any changes to the code require restarting the server (`ctrl-c` and `npm start` again) to see the update.
 
-* Note: changes to the code require restarting the server (`ctrl-c` and `npm start` again).
+----------------------
 ### Botkit Starter Kit
 
-This is a Botkit starter kit for web, created with the [Yeoman generator](https://github.com/howdyai/botkit/tree/master/packages/generator-botkit#readme)
+This project was bootstrapped from the Botkit starter kit for web, created with the [Yeoman generator](https://github.com/howdyai/botkit/tree/master/packages/generator-botkit#readme)
 
 [Botkit Docs](https://botkit.ai/docs/v4)
 
